9b332886385ba0db3c5c5603ac51c067656e382d
milanmenezes <milanmenezes@gmail.com>
2016-08-19 14:52:44 +0530

    email change
    to email: aakar2k16@gmail.com

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Aakar Admin <admin@aakarnie.in>'; 
-$sendTo = 'Aakar Info <info@aakarnie.in>';
+$sendTo = 'Aakar <aakar2k16@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
